8429eafefd6233a8c8a2b4afcad8ba9a9b6d6a44
Quentin ADAM <quentin.adam@waxzce.org>
2011-10-27 12:56:55 +0200

    smooth

--- tw.js
+++ tw.js
@@ -219,7 +219,7 @@ if($.query.get('twrequest') != ''){
     window.setInterval(hearbeat, $().hearbeatTime);
     $('#hashtag').text($().hashtag);
     $('#logo img').attr('src', $().img);
-	if($().imgsmooth){
+	if($.query.get('imgsmooth')){
 		$('#logo').css({'right':'0px','top':'0px'});
 	}
 }else{
